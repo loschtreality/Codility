721de59606eb7369e311f746030a767a3a15acba
Michael Loschiavo <losch103@gmail.com>
2017-03-16 10:54:22 -0700

    equiLeader solution, o(n) time, o(n) space

---
+++ Leader/equileader.js
@@ -0,0 +1,91 @@
+// A non-empty zero-indexed array A consisting of N integers is given.
+
+// The leader of this array is the value that occurs in more than half of the elements of A.
+
+// An equi leader is an index S such that 0 ≤ S < N − 1 and two sequences A[0], A[1], ..., A[S] and A[S + 1], A[S + 2], ..., A[N − 1] have leaders of the same value.
+
+// For example, given array A such that:
+
+    // A[0] = 4
+    // A[1] = 3
+    // A[2] = 4
+    // A[3] = 4
+    // A[4] = 4
+    // A[5] = 2
+// we can find two equi leaders:
+
+// 0, because sequences: (4) and (3, 4, 4, 4, 2) have the same leader, whose value is 4.
+// 2, because sequences: (4, 3, 4) and (4, 4, 2) have the same leader, whose value is 4.
+// The goal is to count the number of equi leaders.
+
+// Write a function:
+
+// function solution(A);
+// that, given a non-empty zero-indexed array A consisting of N integers, returns the number of equi leaders.
+
+// For example, given:
+
+    // A[0] = 4
+    // A[1] = 3
+    // A[2] = 4
+    // A[3] = 4
+    // A[4] = 4
+    // A[5] = 2
+// the function should return 2, as explained above.
+
+// Assume that:
+
+// N is an integer within the range [1..100,000];
+// each element of array A is an integer within the range [−1,000,000,000..1,000,000,000].
+// Complexity:
+
+// expected worst-case time complexity is O(N);
+// expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+// Elements of input arrays can be modified.
+
+const solution = (a) => {
+  let candidate = -1
+  let candidateCount = 0
+
+  a.forEach((num, index) => {
+    if (candidateCount === 0) {
+      candidate = num
+      candidateCount++
+    } else {
+      if (num === candidate) {
+        candidateCount++
+      } else {
+        candidateCount--
+      }
+    }
+  })
+
+  let leaderCount = a.reduce((count, current) => {
+    if (current === candidate) {
+      return count + 1
+    }
+    return count
+  }, 0)
+
+  if (leaderCount <= Math.trunc(a.length / 2)) {
+    return 0
+  }
+
+  let leader = candidate
+  let equiLeaders = 0
+  let currentLeaderCount = 0
+
+  a.forEach((num, index) => {
+    if (num === leader)  currentLeaderCount++
+
+    if (currentLeaderCount > Math.trunc((index + 1) / 2) &&
+      leaderCount - currentLeaderCount > Math.trunc((a.length - index - 1) / 2)) {
+      equiLeaders++
+  }
+  })
+
+  return equiLeaders
+}
+
+const array = [4, 3, 4, 4, 4, 2]
+console.log(solution(array)) // expect 2, got: 2
