98eec0e67aa29a1a06b554cacd901b6c5bc7f551
Michael Loschiavo <losch103@gmail.com>
2017-03-22 10:24:21 -0700

    complete challenges MaxDoubleSlice and MaxProfit

--- Max_Slice/MaxDoubleSlice.js
+++ Max_Slice/MaxDoubleSlice.js
@@ -48,31 +48,76 @@
 // expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
 // Elements of input arrays can be modified.
 
-const slowSolution = (a) => {
-  let sum = 0
+// const slowSolution = (a) => {
+//   let sum = 0
+//
+//   for (let x = 0, len = a.length - 2; x < len; x++) {
+//     for (let y = x + 1, len = a.length - 1; y < len; y++) {
+//       for (let z = y + 1, len = a.length; z < len; z++) {
+//         const sliceSums = a.slice(x + 1, y).concat(a.slice(y + 1, z))
+//         sum = Math.max(sum, maxSlice(sliceSums))
+//       }
+//     }
+//   }
+//
+//   return sum
+// }
 
-  for (let x = 0, len = a.length - 2; x < len; x++) {
-    for (let y = x + 1, len = a.length - 1; y < len; y++) {
-      for (let z = y + 1, len = a.length; z < len; z++) {
-        const sliceSums = a.slice(x + 1, y).concat(a.slice(y + 1, z))
-        sum = Math.max(sum, maxSlice(sliceSums))
-      }
-    }
+// const maxSlice = (a) => {
+//   let maxEnding = 0
+//   let maxSliceSum = 0
+//   a.forEach((num) => {
+//     maxEnding = Math.max(0, maxEnding + num)
+//     maxSliceSum = Math.max(maxEnding, maxSliceSum)
+//   })
+//
+//   return maxSliceSum
+// }
+
+// const solution = (array) => {
+//   if (array.length <= 1) return array[0] || 0
+//
+//   const midPoint = Math.floor(array.length / 2) - 1
+//   const left = array.slice(1, midPoint - 1)
+//   const right = array.slice(midPoint + 1, array.length - 2)
+//
+//   const leftSum = left.reduce((sum, current) => { return sum + current }, 0)
+//   const rightSum = right.reduce((sum, current) => { return sum + current }, 0)
+//
+//   return Math.max(leftSum + rightSum, solution(left) + solution(right))
+// }
+
+// const array = [3, 2, 6, -1, 4, 5, -1, 2]
+// const array2 = [5, -2, 10, 3, -25, 12, 6]
+// console.log(slowSolution(array)) // expect 17, got: 17
+
+// console.log(solution(array))
+// console.log(solution(array)) // 18
+
+
+const solution = (array) => {
+
+  // Generate 2 arrays the size of the original containing zeros
+  const maxEndings = array.map(() => 0)
+  const maxBeginnings = array.map(() => 0)
+
+  for (let i = 1, len = array.length - 1; i < len; i++) {
+    maxEndings[i] = Math.max(maxEndings[i - 1] + array[i], 0)
   }
 
-  return sum
-}
+  for (let j = array.length - 2; j > 0; j--) {
+    maxBeginnings[j] = Math.max(maxBeginnings[j + 1] + array[j], 0)
+  }
 
-const maxSlice = (a) => {
-  let maxEnding = 0
-  let maxSliceSum = 0
-  a.forEach((num) => {
-    maxEnding = Math.max(0, maxEnding + num)
-    maxSliceSum = Math.max(maxEnding, maxSliceSum)
-  })
+  let max = 0
 
-  return maxSliceSum
+  for (let i = 1, len = array.length - 1; i < len; i++) {
+    const maxSum = maxEndings[i - 1] + maxBeginnings[i + 1]
+    max = Math.max(max, maxSum)
+  }
+
+  return max
 }
 
 const array = [3, 2, 6, -1, 4, 5, -1, 2]
-console.log(slowSolution(array)) // expect 17, got: 17
+console.log(solution(array))
